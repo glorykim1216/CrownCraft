99bc0286e7929b6839b81e029ce85c3c1f719be7
glorykim <glrorykim1216@gmail.com>
2017-06-29 09:07:30 +0900

    GameManager 스크립트 수정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    float manaGage
+    List<int> list = new List<int>();
+    float mana;
     // Use this for initialization
     void Start()
     {
+        // List Shuffle
+        for (int i = 0; i < 8; i++)
+        {
+            list.Add(i);
+        }
+        for (int i = 0; i < list.Count; i++)
+        {
+            int rand = Random.Range(0, list.Count);
+            int temp = list[i];
+            list[i] = list[rand];
+            list[rand] = temp;
+        }
+        for (int i = 0; i < list.Count; i++)
+        {
+            Debug.Log(list[i]);
+        }
 
+        // 드레그드롭하면 UIManager호출->
+        // 없어지고(숨기고)
+        // Next카드 날라오고(이미지 Next로 바꿈, 다시 Next카드 이미지 바뀌고)
+        // 다시 생기고(숨긴거 켜줌)
     }
 
     // Update is called once per frame
     void Update()
     {
-        UI_Manager.Instance.SetMana()
+        mana += Time.deltaTime * 0.05f;
+        UI_Manager.Instance.SetMana(mana);
     }
 }
