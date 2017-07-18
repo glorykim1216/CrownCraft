ad4901797b4d5964eb0fd4b353fdcd6175cf05bb
ydhcjswo <ydhcjswo@gmail.com>
2017-07-18 17:52:31 +0900

    별 에니메이션

--- Assets/Scripts/UI/UI_GameOver.cs
+++ Assets/Scripts/UI/UI_GameOver.cs
@@ -4,68 +4,86 @@ using UnityEngine;
 
 public class UI_GameOver : BaseObject
 {
-    UIButton OKBtn = null;
+	UIButton OKBtn = null;
 
-    private void Awake()
-    {
-        // LobbyIcon Btn
-        //----------------------------------------------------
-        Transform trans = FindInChild("OKBtn");
-        if (trans == null)
-        {
-            Debug.LogError("OKBtn is Not Founded");
-            return;
-        }
-        OKBtn = trans.GetComponent<UIButton>();
-        EventDelegate.Add(OKBtn.onClick,
-            new EventDelegate(this, "SceneChange"));
-    }
+	private void Awake()
+	{
+		// LobbyIcon Btn
+		//----------------------------------------------------
+		Transform trans = FindInChild("OKBtn");
+		if (trans == null)
+		{
+			Debug.LogError("OKBtn is Not Founded");
+			return;
+		}
+		OKBtn = trans.GetComponent<UIButton>();
+		EventDelegate.Add(OKBtn.onClick,
+			new EventDelegate(this, "SceneChange"));
+	}
 
-    void SceneChange()
-    {
-        Scene_Manager.Instance.LoadScene(eSceneType.SCENE_LOBBY);
-    }
+	void SceneChange()
+	{
+		Scene_Manager.Instance.LoadScene(eSceneType.SCENE_LOBBY);
+	}
 
-    public void Result(int EnemyTowerDestroyCount, int PlayerTowerDestroyCount)
-    {
-        // Star UI
-        Transform trans = FindInChild("Red");
-        for (int i = 0; i < PlayerTowerDestroyCount; i++)
-        {
-            Transform star = trans.FindChild("Star" + i);
-            star.gameObject.SetActive(true);
-        }
+	public void Result(int EnemyTowerDestroyCount, int PlayerTowerDestroyCount)
+	{
+		// Star UI
+		Transform trans = FindInChild("Red");
+		StartCoroutine(ActiveStar(trans, EnemyTowerDestroyCount, PlayerTowerDestroyCount));
 
-        trans = FindInChild("Blue");
-        for (int i = 0; i < EnemyTowerDestroyCount; i++)
-        {
-            Transform star = trans.FindChild("Star" + i);
-            star.gameObject.SetActive(true);
-        }
+		trans = FindInChild("Blue");
+		StartCoroutine(ActiveStar(trans, EnemyTowerDestroyCount, PlayerTowerDestroyCount));
 
-        // Win UI
-        if (EnemyTowerDestroyCount > PlayerTowerDestroyCount)
-        {
-            trans = FindInChild("Blue").FindChild("Win");
-            trans.gameObject.SetActive(true);
 
-            int CoinValue = PlayerPrefs.GetInt("CoinValue");
-            CoinValue += 120;
-            PlayerPrefs.SetInt("CoinValue", CoinValue);
+		// Win UI
+		if (EnemyTowerDestroyCount > PlayerTowerDestroyCount)
+		{
+			trans = FindInChild("Blue").FindChild("Win");
+			trans.gameObject.SetActive(true);
 
-            int TrophyValue = PlayerPrefs.GetInt("TrophyValue");
-            TrophyValue += 15;
-            PlayerPrefs.SetInt("TrophyValue", TrophyValue);
-        }
-        else if(EnemyTowerDestroyCount < PlayerTowerDestroyCount)
-        {
-            trans = FindInChild("Red").FindChild("Win");
-            trans.gameObject.SetActive(true);
-        }
-        else
-        {
-            // 무승부
-        }
-        
-    }
+			int CoinValue = PlayerPrefs.GetInt("CoinValue");
+			CoinValue += 120;
+			PlayerPrefs.SetInt("CoinValue", CoinValue);
+
+			int TrophyValue = PlayerPrefs.GetInt("TrophyValue");
+			TrophyValue += 15;
+			PlayerPrefs.SetInt("TrophyValue", TrophyValue);
+		}
+		else if (EnemyTowerDestroyCount < PlayerTowerDestroyCount)
+		{
+			trans = FindInChild("Red").FindChild("Win");
+			trans.gameObject.SetActive(true);
+		}
+		else
+		{
+			// 무승부
+		}
+
+	}
+	IEnumerator ActiveStar(Transform trans, int EnemyTowerDestroyCount, int PlayerTowerDestroyCount)
+	{
+		if (trans.name.Equals("Red"))
+		{
+			for (int i = 0; i < PlayerTowerDestroyCount; i++)
+			{
+				Transform star = trans.FindChild("Star" + i);
+
+				star.gameObject.SetActive(true);
+				yield return new WaitForSeconds(0.4f);
+			}
+		}
+		else
+		{
+			for (int i = 0; i < EnemyTowerDestroyCount; i++)
+			{
+				Transform star = trans.FindChild("Star" + i);
+
+				star.gameObject.SetActive(true);
+				yield return new WaitForSeconds(0.4f);
+			}
+		}
+
+
+	}
 }
---
+++ Assets/UI_ScaleAnimation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ScaleAnimation: MonoBehaviour
+{
+
+	float duration = 0.4f; // 애니메이션의 길이입니다.(시간)
+	float startDelay = 0.2f; // 애니메이션 시작 전 딜레이입니다.
+	Vector3 scaleTo = new Vector3(1f, 1f, 1f); // 오브젝트의 최종 Scale 입니다.
+
+	// 부풀었다가 줄어드는 효과를 위한 AnimationCurve 입니다.
+	AnimationCurve animationCurve = new AnimationCurve(
+	new Keyframe(0f, 0f, 0f, 1f), // 0%일때 0의 값에서 시작해서 
+	new Keyframe(0.7f, 1.2f, 1f, 1f), // 애니메이션 시작후 70% 지점에서 1.2의 사이즈까지 커졌다가 
+	new Keyframe(1f, 1f, 1f, 0f)); // 100%로 애니메이션이 끝날때는 1.0의 사이즈가 됩니다.
+
+	void Awake()
+	{
+		this.transform.localScale = new Vector3(0, 0, 0);
+	}
+
+	//SetActive(true) 호출 시 실행됩니다.
+	void OnEnable()
+	{
+		open();
+	}
+
+	// 팝업 열기
+	void open()
+	{
+		init();
+
+	}
+
+
+	// 초기화
+	void init()
+	{
+		transform.localScale = new Vector3(0, 0, 0); 
+		TweenScale tween = TweenScale.Begin(gameObject, duration, scaleTo);
+		tween.duration = duration;
+		tween.delay = startDelay;
+		//tween.method = UITweener.Method.BounceIn; // AnimationCurve 대신 이것도 한번 써보세요.
+		tween.animationCurve = animationCurve;
+
+	}
+
+	// 팝업 닫기
+	public void close()
+	{
+		setDisable();
+	}
+
+	// 오브젝트 Disable
+	void setDisable()
+	{
+		gameObject.transform.localScale = new Vector3(0, 0, 0);
+		gameObject.SetActive(false);
+	}
+}
+
